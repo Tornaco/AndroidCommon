01a8153fdf924424dc849f404c3037d950f875cf
tornaco <tornaco@163.com>
2017-09-18 12:57:31 +0800

    Init projects

---
+++ common/src/main/java/github/tornaco/android/common/Holder.java
@@ -0,0 +1,19 @@
+package github.tornaco.android.common;
+
+/**
+ * Created by Nick@NewStand.org on 2017/4/15 13:39
+ * E-Mail: NewStand@163.com
+ * All right reserved.
+ */
+public class Holder<T> {
+
+    private T data;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
